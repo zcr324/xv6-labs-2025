25f0d7dd3e931f9c805ec25255550f35555b268c
zcr324 <zhaoyicheng1419@163.com>
2026-09-08 23:32:18 +0800

    find -exec

--- user/find.c
+++ user/find.c
@@ -1,5 +1,6 @@
 #include "kernel/types.h"
 #include "kernel/stat.h"
+#include "kernel/param.h"
 #include "user/user.h"
 #include "kernel/fs.h"
 #include "kernel/fcntl.h"
@@ -16,7 +17,30 @@ filename(char *path)
 }
 
 void
-find(char *path, char *target)
+runexec(char **cmd, char *path)
+{
+  char *args[MAXARG];
+  int i;
+
+  for(i = 0; cmd[i] != 0 && i < MAXARG - 2; i++)
+    args[i] = cmd[i];
+
+  args[i++] = path;
+  args[i] = 0;
+
+  if(fork() == 0){
+    exec(args[0], args);
+
+    // 只有 exec 失败才会执行到这里
+    fprintf(2, "find: exec %s failed\n", args[0]);
+    exit(1);
+  }
+
+  wait(0);
+}
+
+void
+find(char *path, char *target, char **cmd)
 {
   char buf[512], *p;
   int fd;
@@ -38,8 +62,13 @@ find(char *path, char *target)
 
   case T_DEVICE:
   case T_FILE:
-    if(strcmp(filename(path), target) == 0)
-      printf("%s\n", path);
+    if(strcmp(filename(path), target) == 0){
+      if(cmd == 0){
+        printf("%s\n", path);
+      } else {
+        runexec(cmd, path);
+      }
+    }
     break;
 
   case T_DIR:
@@ -53,18 +82,21 @@ find(char *path, char *target)
     *p++ = '/';
 
     while(read(fd, &de, sizeof(de)) == sizeof(de)){
-
       if(de.inum == 0)
         continue;
 
-      if(strcmp(de.name, ".") == 0 ||
-         strcmp(de.name, "..") == 0)
+      char name[DIRSIZ + 1];
+
+      memmove(name, de.name, DIRSIZ);
+      name[DIRSIZ] = '\0';
+
+      if(strcmp(name, ".") == 0 ||
+         strcmp(name, "..") == 0)
         continue;
 
-      memmove(p, de.name, DIRSIZ);
-      p[DIRSIZ] = '\0';
+      strcpy(p, name);
 
-      find(buf, target);
+      find(buf, target, cmd);
     }
 
     break;
@@ -76,12 +108,23 @@ find(char *path, char *target)
 int
 main(int argc, char *argv[])
 {
-  if(argc != 3){
-    fprintf(2, "usage: find path filename\n");
+  if(argc < 3){
+    fprintf(2, "usage: find path filename [-exec cmd ...]\n");
     exit(1);
   }
 
-  find(argv[1], argv[2]);
+  // 普通 find
+  if(argc == 3){
+    find(argv[1], argv[2], 0);
+    exit(0);
+  }
 
-  exit(0);
+  // find ... -exec ...
+  if(argc >= 5 && strcmp(argv[3], "-exec") == 0){
+    find(argv[1], argv[2], &argv[4]);
+    exit(0);
+  }
+
+  fprintf(2, "usage: find path filename [-exec cmd ...]\n");
+  exit(1);
 }
